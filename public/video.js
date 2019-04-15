fb36303b4f138a77e901c4b0074a94dbcab2bfad
tzwl <thinzar.winlae@gmail.com>
2019-04-16 02:22:41 +0630

    update

--- public/video.js
+++ public/video.js
@@ -1,6 +1,6 @@
 // Make connection
 // var socket = io.connect('http://localhost:4000');
-var socket = io.connect('https://my-coolapp.herokuapp.com/');
+var socket = io.connect('https://syncvideoplayer.herokuapp.com/');
 
 
 var play = document.getElementById('play'),
